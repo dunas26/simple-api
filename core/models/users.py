04ecc98caf7c17098a58bb2dea3ad3699d81b0d5
dunas26 <jhonjamesillo@gmail.com>
2022-12-01 17:49:07 -0500

    update database field to match datetime

--- core/models/users.py
+++ core/models/users.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column
-from sqlalchemy.types import String, Date
+from sqlalchemy.types import String, DateTime
 from core.models import Base
 
 
@@ -7,4 +7,4 @@ class User(Base):
     username = Column(String(255))
     email = Column(String(128))
     password = Column(String(64))
-    created_at = Column(Date())
+    created_at = Column(DateTime())
